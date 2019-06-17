31f346bcb8622ad4ceb9195622537116da5dd967
HeBo <hebomou@qq.com>
2019-06-17 17:45:00 +0800

    排片修完了

--- src/views/AdminMovieSchedule.vue
+++ src/views/AdminMovieSchedule.vue
@@ -1,132 +1,160 @@
 <template>
   <Card style="width:1000px;padding-left: 80px">
-    <Form  inline style="padding-left: 20px">
+    <Form inline style="padding-left: 20px">
       <FormItem label="选择电影" style="width:200px;margin-right: 150px;text-align: left">
-        <Select v-model="movieModel" >
-          <Option v-for="item in movieList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Select v-model="movieModel">
+          <Option v-for="item in movieList" :value="item.id" :key="item.id">{{ item.name }}</Option>
         </Select>
       </FormItem>
-      <FormItem label="选择日期" style="width:200px;margin-right: 150px" >
+      <FormItem label="选择日期" style="width:200px;margin-right: 150px">
         <Row>
-          <Col span="20" >
-            <DatePicker type="daterange" :options="options2" placement="bottom-end" placeholder="请选择日期" style="width: 200px"></DatePicker>
+          <Col span="20">
+            <DatePicker
+              type="daterange"
+              :options="options2"
+              placement="bottom-end"
+              placeholder="请选择日期"
+              style="width: 200px"
+            ></DatePicker>
           </Col>
         </Row>
       </FormItem>
       <FormItem label="选择影厅" style="width:200px;margin-right: 150px;text-align: left">
-        <Select v-model="hallModel" >
-          <Option v-for="item in hallList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Select v-model="hallModel">
+          <Option v-for="item in hallList" :value="item.id" :key="item.id">{{ item.name }}</Option>
         </Select>
       </FormItem>
       <FormItem label="场次票价" style="width:200px;margin-right: 150px">
-        <Input v-model="ticketValue" prefix="logo-yen" placeholder="请输入数字" style="width: 200px" type ="number"/>
+        <Input
+          v-model="ticketValue"
+          prefix="logo-yen"
+          placeholder="请输入数字"
+          style="width: 200px"
+          type="number"
+        />
       </FormItem>
       <FormItem label="电影时间" style="width:400px;margin-right: 300px;text-align: left">
         <el-time-picker
           is-range
           arrow-control
           size="small"
-          default-value=""
+          default-value
           v-model="timeValue"
           range-separator="-"
           start-placeholder="开始时间"
           end-placeholder="结束时间"
-          placeholder="选择时间范围">
-        </el-time-picker>
+          placeholder="选择时间范围"
+        ></el-time-picker>
       </FormItem>
-<Form>
-  <FormItem >
-    <Button type="primary" @click="addSchedule" style="margin-right: 50px">确认</Button>
-    <Button @click="handleReset('formValidate')" style="margin-right: 180px">重置</Button>
-  </FormItem>
-</Form>
-
+      <Form>
+        <FormItem>
+          <Button type="primary" @click="addSchedule" style="margin-right: 50px">确认</Button>
+          <Button @click="handleReset('formValidate')" style="margin-right: 180px">重置</Button>
+        </FormItem>
+      </Form>
     </Form>
-
   </Card>
-
-
 </template>
 
 <script>
-  import axios from "axios"
-  export default {
-    name: "adminMovieSchedule",
-    data() {
-      return {
-        movieList: [
+import axios from "axios";
+export default {
+  name: "adminMovieSchedule",
+  data() {
+    return {
+      movieList: [
+        {
+          name: "fdsf",
+          id: 123
+        }
+      ],
+      hallList: [
+        {
+          name: "fdsfd",
+          id: "fsdfds"
+        }
+      ],
+      movieModel: "",
+      hallModel: "",
+      options2: {
+        shortcuts: [
           {
-            value: 'quanlide ',
-            label: '权力的游戏',
-          }
-        ],
-        hallList: [
-          {
-            value: 'first',
-            label: '一号厅',
-          }
-        ],
-        movieModel: '',
-        hallModel: '',
-        options2: {
-          shortcuts: [
-            {
-              text: '1 week',
-              value() {
-                const end = new Date();
-                const start = new Date();
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-                return [start, end];
-              }
-            },
-            {
-              text: '1 month',
-              value() {
-                const end = new Date();
-                const start = new Date();
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-                return [start, end];
-              }
-            },
-            {
-              text: '3 months',
-              value() {
-                const end = new Date();
-                const start = new Date();
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                return [start, end];
-              }
+            text: "1 week",
+            value() {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              return [start, end];
             }
-          ]
-        },
-        ticketValue: '',
-        timeValue:  '',
-      }
-
-    },
-    methods:{
-      addSchedule: function () {
-        //前端给后端的输入
-        var addScheduleList = {
-          hallName: this.hallModel,
-          movieName: this.movieModel,
-          startTime: this.timeValue[0],
-          endTime: this.timeValue[1],
-          fare: this.ticketValue,
-        };
-        console.log(addScheduleList);
-        axios.post('http://172.28.193.125:8080/InsertSchedule',addScheduleList)
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-      }
+          },
+          {
+            text: "1 month",
+            value() {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              return [start, end];
+            }
+          },
+          {
+            text: "3 months",
+            value() {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              return [start, end];
+            }
+          }
+        ]
+      },
+      ticketValue: "",
+      timeValue: ""
+    };
+  },
+  methods: {
+    addSchedule: function() {
+      //前端给后端的输入
+      var addScheduleList = {
+        hallId: this.hallModel,
+        movieId: this.movieModel,
+        startTime: this.timeValue[0],
+        endTime: this.timeValue[1],
+        price: this.ticketValue
+      };
+      console.log(addScheduleList);
+      axios
+        .post("InsertSchedule", addScheduleList)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
+  },
+  created() {
+    let _this = this;
+    axios
+      .get("GetHallList")
+      .then(function(response) {
+        console.log(response.data);
+        _this.hallList = response.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    axios
+      .get("GetMovieList")
+      .then(function(response) {
+        console.log(response.data);
+        _this.movieList = response.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/views/AdminScheduleList.vue
+++ src/views/AdminScheduleList.vue
@@ -4,14 +4,14 @@
       <Col span="7">
         <FormItem label="选择影厅" style="width: 250px">
           <Select v-model="formItem.selectHall">
-            <Option v-for="item in hallNameList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            <Option v-for="item in hallList" :value="item.id" :key="item.id">{{ item.name }}</Option>
           </Select>
         </FormItem>
       </Col>
       <Col span="6">
         <FormItem label="选择电影" style="width: 250px">
           <Select v-model="formItem.selectMovie">
-            <Option v-for="item in MovieNameList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            <Option v-for="item in MovieList" :value="item.id" :key="item.id">{{ item.name }}</Option>
           </Select>
         </FormItem>
       </Col>
@@ -20,202 +20,199 @@
     <FormItem label="选择日期">
       <Row>
         <Col span="5">
-          <DatePicker type="daterange" confirm placement="bottom-end" placeholder="请选择日期范围"
-                      v-model="formItem.date"></DatePicker>
+          <DatePicker
+            type="daterange"
+            confirm
+            placement="bottom-end"
+            placeholder="请选择日期范围"
+            v-model="formItem.date"
+          ></DatePicker>
         </Col>
         <Col span="1" style="text-align: center">-</Col>
         <Col span="5">
-          <TimePicker format="HH:mm" type="timerange" placement="bottom-end"
-                      placeholder="请选择时间范围（可选）" v-model="formItem.time" style="width: 168px"></TimePicker>
+          <TimePicker
+            format="HH:mm"
+            type="timerange"
+            placement="bottom-end"
+            placeholder="请选择时间范围（可选）"
+            v-model="formItem.time"
+            style="width: 168px"
+          ></TimePicker>
         </Col>
       </Row>
     </FormItem>
     <div class="card" style="margin-top: 20px">
       <div class="schedule-date-container">
-        <div class="schedule-date" v-for="item in dateList" :value="item.value" :key="item.value">{{ item.label }}</div>
-        <Option v-for="item in hallNameList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <div
+          class="schedule-date"
+          v-for="item in dateList"
+          :value="item.value"
+          :key="item.value"
+        >{{ item.label }}</div>
+        <Option v-for="item in hallList" :value="item.id" :key="item.id">{{ item.name }}</Option>
       </div>
       <div class="schedule-container">
-        <ul class="schedule-time-line">2019/9/1
+        <ul class="schedule-time-line">
+          2019/9/1
           <li class="schedule-time-item">00:00</li>
           <li class="schedule-time-item">06:00</li>
           <li class="schedule-time-item">12:00</li>
           <li class="schedule-time-item">18:00</li>
           <li class="schedule-time-item">24:00</li>
         </ul>
-        <ul class="schedule-item-line">
-        </ul>
+        <ul class="schedule-item-line"></ul>
       </div>
     </div>
   </Form>
 </template>
 
 <script>
-  import adminApi from '../api/adminApi'
-  import axios from 'axios'
+import adminApi from "../api/adminApi";
+import axios from "axios";
 
-  export default {
-    name: "AdminScheduleList",
-    data() {
-      return {
-        formItem: {
-          selectHall: '',
-          selectMovie: '',
-          date: [],
-          time: [],
-        },
-
-        dateList:[
-          {
-            value:'',
-            label:'',
-          }
-        ],
-
-        //前端给后端排片搜索条件
-        searchCondition:[
-          {
-            startDate: 2019,
-            endDate: 2020,
-            hallId:1,
-            movieId:112,
-          }
-        ],
-
-
-        //后端要首先给前端影厅和电影名列表
-        hallNameList: [
-          {
-            value: '',
-            label: '',
-          }
-        ],
-        MovieNameList: [
-          {
-            value: '',
-            label: '',
-          }
-        ],
-
-
-
-        //给完搜索条件后，后端给前端的数据
-        retList: [
-          {
-            id: '',   //排片ID
-            hallId: '',
-            hallName: '',
-            movieId: '',
-            movieName: '',
-            startTime: '',
-            endTime: '',
-            fare: '',
-          }
-        ],
-      }
-    },
-
-    methods: {
-      askForHall() {
-        var _this = this;
-        axios.get('http://172.28.202.212:8080/GetHall')
-          .then(function (response) {
-            _this.data = response;
-            for (var i = 0; i < _this.data.length; i++) {
-              _this.hallNameList.push({
-                label: _this.data[i].name,
-                value: _this.data[i].id,
-              })
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
+export default {
+  name: "AdminScheduleList",
+  data() {
+    return {
+      formItem: {
+        selectHall: "",
+        selectMovie: "",
+        date: [],
+        time: []
       },
-      askForMovie() {
-        var _this = this;
-        axios.get('http://172.28.202.212:8080/GetMovieList')
-          .then(function (response) {
-            _this.data = response;
-            for (var i = 0; i < _this.data.length; i++) {
-              _this.MovieNameList.push({
-                label: _this.data[i].name,
-                value: _this.data[i].id,
-              })
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-      },
-      askForSchedule() {
-        var dataToPost =  [
-          {
-            startDate: this.formItem.date[0],
-            endDate: this.formItem.date[1],
-            startTime: this.formItem.time[0],
-            endTime: this.formItem.time[1],
-            hallId: this.formItem.selectHall,
-            movieId: this.formItem.selectMovie,
-          },
-        ];
-        var _this = this;
-        axios.post('http://172.28.202.212:8080/GetScheduleList', dataToPost)
-          .then(function (response) {
-            _this.data = response;
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-      },
-    },
-    /*
-created() {
-      askForHall();
-}
-*/
+
+      dateList: [
+        {
+          value: "",
+          label: ""
+        }
+      ],
+
+      //前端给后端排片搜索条件
+      searchCondition: [
+        {
+          startDate: 2019,
+          endDate: 2020,
+          hallId: 1,
+          movieId: 112
+        }
+      ],
+
+      //后端要首先给前端影厅和电影名列表
+      hallList: [
+        {
+          name: "",
+          id: ""
+        }
+      ],
+      MovieList: [
+        {
+          name: "",
+          id: ""
+        }
+      ],
+
+      //给完搜索条件后，后端给前端的数据
+      retList: [
+        {
+          id: "", //排片ID
+          hallId: "",
+          hallName: "",
+          movieId: "",
+          movieName: "",
+          startTime: "",
+          endTime: "",
+          fare: ""
+        }
+      ]
+    };
+  },
+
+  methods: {
+    askForSchedule() {
+      var dataToPost = [
+        {
+          startDate: this.formItem.date[0],
+          endDate: this.formItem.date[1],
+          startTime: this.formItem.time[0],
+          endTime: this.formItem.time[1],
+          hallId: this.formItem.selectHall,
+          movieId: this.formItem.selectMovie
+        }
+      ];
+      var _this = this;
+      axios
+        .post("http://172.28.202.212:8080/GetScheduleList", dataToPost)
+        .then(function(response) {
+          _this.data = response;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  },
+  created() {
+    var _this = this;
+    axios
+      .get("GetHallList")
+      .then(function(response) {
+        console.log(response.data);
+        _this.hallList = response.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    axios
+      .get("GetMovieList")
+      .then(function(response) {
+        console.log (response.data)
+        _this.MovieList = response.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
+};
 </script>
 <style scoped>
-  .schedule-container {
-    display: flex;
-    padding: 0 20px;
-  }
+.schedule-container {
+  display: flex;
+  padding: 0 20px;
+}
 
-  .schedule-time-line {
-    width: 80px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
+.schedule-time-line {
+  width: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 
-  .schedule-item-line {
-    height: 960px;
-    width: 150px;
-    border-left: 1px solid #ccc;
-    position: relative;
-  }
+.schedule-item-line {
+  height: 960px;
+  width: 150px;
+  border-left: 1px solid #ccc;
+  position: relative;
+}
 
-  .schedule-item {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: #fff;
-    position: absolute;
-    overflow-y: scroll;
-  }
+.schedule-item {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #fff;
+  position: absolute;
+  overflow-y: scroll;
+}
 
-  .schedule-date-container {
-    padding-left: 100px;
-    height: 40px;
-    line-height: 30px;
-    display: flex;
-  }
+.schedule-date-container {
+  padding-left: 100px;
+  height: 40px;
+  line-height: 30px;
+  display: flex;
+}
 
-  .schedule-date {
-    width: 150px;
-    text-align: center;
-  }
-
+.schedule-date {
+  width: 150px;
+  text-align: center;
+}
 </style>
